aa04bb8931d0b574cf3ff75d9e4c3e505233be27
Ivansyah Eka Oktaviadi Santoso <143073523+I4annet@users.noreply.github.com>
2026-02-25 10:27:18 +0700

    Delete Pertemuan 3 directory

--- Pertemuan 3/src/pages/profile/edit.tsx
+++
@@ -1,5 +0,0 @@
-const ProfileEditPage = () => {
-  return <h1>Halaman Edit Profile</h1>;
-};
-
-export default ProfileEditPage;
--- Pertemuan 3/src/pages/profile/index.tsx
+++
@@ -1,5 +0,0 @@
-const ProfilePage = () => {
-  return <h1>Halaman Profile</h1>;
-};
-
-export default ProfilePage;
