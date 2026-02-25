2570cb5d612e54b4d92f3d6af9b251a7f9fa8505
Ivansyah Eka Oktaviadi Santoso <ivansyahsantoso@gmail.com>
2026-02-25 11:07:05 +0700

    Tugas 8 - Pengayaan ✅

--- Pertemuan 3/src/pages/_app.tsx
+++ Pertemuan 3/src/pages/_app.tsx
@@ -2,8 +2,19 @@ import AppShell from '@/components/layouts/Appshell'
 import Navbar from '@/components/layouts/navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/dist/client/components/navigation';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }
+    , [isLogin, push]);
+    
   return (
       <div>
       <AppShell>
